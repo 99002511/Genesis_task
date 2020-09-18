8524ce23bc628964df4c5d693fd532fb52297656
99002529 <71481157+99002529@users.noreply.github.com>
2020-09-18 22:22:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-    printf("Choose a operation you want from 1 to 100 /n");
-    int selectioin;
+    printf("Choose a operation you want from 1 to 100 \n");
+    int selection;
     scanf("%d",&selection);
-    printf("Type 2 numbers");
+    printf("Type 2 numbers\n");
     int num1,num2,output;
     scanf("%d%d",&num1,&num2);
     switch(selection)
     {
     case 1:
-        outut=add(num1,num2);
+        output=add(num1,num2);
         break;
     case 2:
         output=sub(num1,num2);
