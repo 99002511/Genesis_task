334f1f64a4d1833236ea31ba5a05fe7d1a518880
99002529 <71481157+99002529@users.noreply.github.com>
2020-09-18 22:17:06 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    printf("Choose a operation you want from 1 to 100 /n");
+    int selectioin;
+    scanf("%d",&selection);
+    printf("Type 2 numbers");
+    int num1,num2,output;
+    scanf("%d%d",&num1,&num2);
+    switch(selection)
+    {
+    case 1:
+        outut=add(num1,num2);
+        break;
+    case 2:
+        output=sub(num1,num2);
+        break;
+    case 3:
+        output=mult(num1,num2);
+        break;
+    }
+
+
+    printf("%d",output);
+}
