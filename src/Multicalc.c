9801384a121af89a814d2388d995865362db924e
99002529 <71481157+99002529@users.noreply.github.com>
2020-09-18 22:18:24 +0530

    Update Multicalc.c

--- src/Multicalc.c
+++ src/Multicalc.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "func.h"
+
+int add(int num1,int num2)
+{
+    return num1+num2;
+}
+
+int sub(int num1, int num2)
+{
+    return num1-num2;
+}
+
+int mult(int num1,int num)
+{
+    return num1*num2;
+}
 
