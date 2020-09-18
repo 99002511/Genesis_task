8f53d12d1b6358a17f0ecec15b8b8a1e0ef55bea
99002529 <71481157+99002529@users.noreply.github.com>
2020-09-19 00:16:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
 
 int main()
 {
-    printf("Choose a operation you want from 1 to 100 \n");
+    printf("Choose a operation you want from 1 to 23\n");
     int selection;
     scanf("%d",&selection);
-    printf("Type 2 numbers\n");
     int num1,num2,output;
-    scanf("%d%d",&num1,&num2);
+    if(selection<5)
+    {
+      printf("Type 2 numbers\n");
+      scanf("%d%d",&num1,&num2);
+    }
+    else
+    {
+        printf("Type 1 number\n");
+        scanf("%d",&num1);
+    }
+
     switch(selection)
     {
     case 1:
@@ -21,8 +30,68 @@ int main()
     case 3:
         output=mult(num1,num2);
         break;
+    case 4:
+        output=divide(num1,num2);
+        break;
+    case 5:
+        output=power(num1,num2);
+        break;
+    case 6:
+        output=cm_inch(num1);
+        break;
+    case 7:
+        output=inch_cm(num1);
+        break;
+    case 8:
+        output=feat_metre(num1);
+        break;
+    case 9:
+        output=metre_feat(num1);
+        break;
+    case 10:
+        output=yard_metre(num1);
+        break;
+    case 11:
+        output=metre_yard(num1);
+        break;
+    case 12:
+        output=mile_km(num1);
+        break;
+    case 13:
+        output=km_mile(num1);
+        break;
+    case 14:
+        output=mile_metre(num1);
+        break;
+    case 15:
+        output=metre_mile(num1);
+        break;
+    case 16:
+        output=acre_sqrmtr(num1);
+        break;
+    case 17:
+        output=sqrmtr_acre(num1);
+        break;
+    case 18:
+        output=galon_litre(num1);
+        break;
+    case 19:
+        output=litre_galon(num1);
+        break;
+    case 20:
+        output=kmph_mps(num1);
+        break;
+    case 21:
+        output=mps_kmph(num1);
+        break;
+    case 22:
+        output=kilogram_pounds(num1);
+        break;
+    case 23:
+        output=pounds_kilogram(num1);
+        break;
+
     }
-
-
     printf("%d",output);
 }
+
