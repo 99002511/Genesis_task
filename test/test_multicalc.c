640c0d60fa6b7600924addab47b6ca1eae7a4374
99002529 <71481157+99002529@users.noreply.github.com>
2020-09-19 11:09:57 +0530

    Update test_multicalc.c

--- test/test_multicalc.c
+++ test/test_multicalc.c
@@ -129,6 +129,51 @@ void test_feat_metre(void) {
 void test_metre_feat(void) {
   CU_ASSERT(16.4042 == metre_feat(5));  
 }
-          
-
-
+void test_metre_yard(void) {
+  CU_ASSERT(5.46807 == metre_yard(5));  
+}
+void test_yard_metre(void) {
+  CU_ASSERT(4.572 == yard_metre(5));  
+}        
+void test_km_mile(void) {
+  CU_ASSERT(3.10686 == km_mile(5));  
+}        
+void test_mile_km(void) {
+  CU_ASSERT(8.04672 == mile_km(5));  
+}     
+void test_metre_mile(void) {
+  CU_ASSERT(0.00310686 == metre_mile(5));  
+}
+void test_mile_metre(void) {
+  CU_ASSERT(8046.72 == mile_metre(5));  
+}
+void test_acre_sqrmtr(void) {
+  CU_ASSERT(20234.3 == acre_sqrmtr(5));  
+}
+void test_sqrmtr_acre(void) {
+  CU_ASSERT(0.00123553 == sqrmtr_acre(5));  
+}
+void test_galon_litre(void) {
+  CU_ASSERT(18.9271 == galon_litre(5));  
+}
+void test_litre_galon(void) {
+  CU_ASSERT(1.32086 == litre_galon(5));  
+}
+void test_kmph_mps(void) {
+  CU_ASSERT(1.38889 == kmph_mps(5));  
+}
+void test_mps_kmph(void) {
+  CU_ASSERT(18 == mps_kmph(5));  
+}
+void test_kilogram_pounds(void) {
+  CU_ASSERT(11.0231 == kilogram_pounds(5));  
+}
+void test_pounds_kilogram(void) {
+  CU_ASSERT(2.26796 == pounds_kilogram(5));  
+}
+void test_KilometresPerLitre_MilesPerLitre(void) {
+  CU_ASSERT(11.7607 == KilometresPerLitre_MilesPerLitre(5));  
+}
+void test_MilesPerLitre_KilometresPerLitre(void) {
+  CU_ASSERT(2.12572 == MilesPerLitre_KilometresPerLitre(5));  
+}
